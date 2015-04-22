8d1a9f7c50910fb04f04b4be2f8b0798b659e42e
Sluip <sluip@sp00fed.dk>
2015-04-22 23:37:33 +0200

    Using example mousetracker class

--- TrackBall/Program.cs
+++ TrackBall/Program.cs
@@ -2,13 +2,26 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Windows.Forms;
+
+using MouseKeyboardActivityMonitor.WinApi;
+
+
 
 namespace TrackBall
 {
     class Program
     {
+        MouseListener gay = new MouseListener(); 
+
+        [STAThread]
         static void Main(string[] args)
         {
+            gay.Activate();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
         }
     }
 }
