96c900982f963583890d5fae0c76396c3c813507
Sluip <sluip@sp00fed.dk>
2015-04-23 01:30:49 +0200

    Added listener for movement, need to separate 2 devices

--- TrackBall/MouseListener.cs
+++ TrackBall/MouseListener.cs
@@ -23,6 +23,7 @@ namespace TrackBall
             // Set the event handler
             // recommended to use the Extended handlers, which allow input suppression among other additional information
             m_mouseListener.MouseDownExt += MouseListener_MouseDownExt;
+            m_mouseListener.MouseMoveExt += MouseListener_MouseMoveExt;
         }
 
         public void Deactivate()
@@ -33,10 +34,15 @@ namespace TrackBall
         private void MouseListener_MouseDownExt(object sender, MouseEventExtArgs e)
         {
             // log the mouse click
-            Console.WriteLine(string.Format("MouseDown: \t{0}; \t System Timestamp: \t{1}", e.Button, e.Timestamp));
+            Console.WriteLine(string.Format("MouseDown: \t{0}; \t System Timestamp: \t{1}", e.WheelScrolled, e.Timestamp));
+
 
             // uncommenting the following line with suppress a middle mouse button click
             // if (e.Buttons == MouseButtons.Middle) { e.Handled = true; }
+        }
+        private void MouseListener_MouseMoveExt(object sender, MouseEventExtArgs e)
+        {
+            Console.WriteLine("Fuck off");
         }
     }
 
