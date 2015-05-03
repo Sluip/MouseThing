aa389b6a949b8e10825067d6851f52ebf6bbc931
Sluip <sluip@sp00fed.dk>
2015-05-03 20:07:37 +0200

    Can't get mouse to target hWnd of Maya

--- TrackBall/Form1.cs
+++ TrackBall/Form1.cs
@@ -1,30 +1,56 @@
 ﻿using System;
 using RawInput_dll;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Diagnostics;
-using System.Globalization;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
 
 namespace TrackBall
 {
     public partial class Form1 : Form
     {
         private readonly RawInput _rawinput;
+        private MouseListener _mouseListener;
+
+        private bool _rotation;
+
         public Form1()
         {
             InitializeComponent();
+            _rotation = true;
+            _mouseListener = new MouseListener();
 
-            _rawinput = new RawInput(Handle, false);
+            IntPtr hwnd = _mouseListener.WinGetHandle("Autodesk Maya 2015");
+            _rawinput = new RawInput(hwnd, true);
 
             _rawinput.AddMessageFilter();   // Adding a message filter will cause keypresses to be handled
-         // Writes a file DeviceAudit.txt to the current directory
-
             Win32.DeviceAudit();
-            _rawinput.KeyPressed += OnKeyPressed;   
+
+            _rawinput.KeyPressed += OnKeyPressed;
         }
         private void OnKeyPressed(object s, RawInputEventArg e)
         {
-            if (e.KeyPressEvent.DeviceName.StartsWith(@"\\?\HID#VID_046D&PID_C52B&MI"))
-                Console.WriteLine("Fuck yeah");
+            //Console.WriteLine(e.KeyPressEvent.DeviceName);
+            if (e.KeyPressEvent.DeviceName.StartsWith(@"\\?\HID#VID_046D&PID_C52B&MI") && e.KeyPressEvent.Message == 0)
+            {
+                if (_rotation)
+                {
+                    _mouseListener.Activate(e.KeyPressEvent.MouseX, e.KeyPressEvent.MouseY);
+                    _rotation = false;
+                }
+
+                Console.WriteLine("Rotating you fuck");
+            }
+            else if (e.KeyPressEvent.DeviceName != "Global Keyboard" && !_rotation)
+            {
+                _mouseListener.DeState(e.KeyPressEvent.MouseX, e.KeyPressEvent.MouseY);
+                _rotation = true;
+                Console.WriteLine(e.KeyPressEvent.KeyPressState);
+                
+            }
         }
     }
 }
--- TrackBall/MouseListener.cs
+++ TrackBall/MouseListener.cs
@@ -4,8 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Diagnostics;
 using System.Threading.Tasks;
-using MouseKeyboardActivityMonitor;
-using MouseKeyboardActivityMonitor.WinApi;
+using System.Threading;
 using System.Runtime.InteropServices;
 using RawInput_dll;
 
@@ -14,44 +13,31 @@ namespace TrackBall
 
     class MouseListener
     {
-        private MouseHookListener m_mouseListener;
-        private KeyboardHookListener m_keyboard;
+
+        //private MouseHookListener m_mouseListener;
+        //private KeyboardHookListener m_keyboard;
         private int g;
         private bool thing;
-        [DllImport("user32.dll", EntryPoint = "FindWindowEx")]
-        public static extern int FindWindowEx(int hwndParent, int hwndEnfant, int lpClasse, string lpTitre);
-        [DllImport("user32.dll", CharSet = CharSet.Auto)]
-        private static extern int GetWindowText(int hWnd, StringBuilder title, int size);
+        //[DllImport("user32.dll", EntryPoint = "FindWindowEx")]
+        //public static extern int FindWindowEx(int hwndParent, int hwndEnfant, int lpClasse, string lpTitre);
+        //[DllImport("user32.dll", CharSet = CharSet.Auto)]
+        //private static extern int GetWindowText(int hWnd, StringBuilder title, int size);
 
 
-        public void Activate()
+        public void Activate(int x, int y)
         {
-            thing = false;
-            // Note: for an application hook, use the AppHooker class instead
-            //m_mouseListener = new MouseHookListener(new GlobalHooker());
-            // The listener is not enabled by default
-            //m_mouseListener.Enabled = true;
-
-           IntPtr hwnd = Process.GetCurrentProcess().MainWindowHandle;
-            //IntPtr hwnd = WinGetHandle("Maya");
-
-            Console.WriteLine(hwnd.ToString());
-
-            RawInput rawinput = new RawInput(hwnd, false);
-            rawinput.AddMessageFilter();
-            rawinput.KeyPressed += OnKeyPressed;
-
-            // Set the event handler
-            // recommended to use the Extended handlers, which allow input suppression among other additional information
-           // m_mouseListener.MouseDownExt += MouseListener_MouseDownExt;
-           // m_mouseListener.MouseMoveExt += MouseListener_MouseMoveExt;
+            
+            keybd_event(0x12, 0xb8, 0, 0);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, (uint)x, (uint)y, 0, 0);
         }
 
-        public void Deactivate()
+        public void DeState(int x, int y)
         {
-            m_mouseListener.Dispose();
+            mouse_event(MOUSEEVENTF_LEFTUP, (uint)x, (uint)y, 0, 0);
+            keybd_event(0x12, 0xb8, KEYEVENTF_KEYUP, 0);
         }
-        public static IntPtr WinGetHandle(string wName)
+
+        public IntPtr WinGetHandle(string wName)
         {
             IntPtr hWnd = IntPtr.Zero;
             foreach (Process pList in Process.GetProcesses())
@@ -59,36 +45,11 @@ namespace TrackBall
                 if (pList.MainWindowTitle.Contains(wName))
                 {
                     hWnd = pList.MainWindowHandle;
+                    Debug.WriteLine(pList.Handle);
 
                 }
             }
             return hWnd;
-        }
-        private void OnKeyPressed(object s, RawInputEventArg e)
-        {
-
-
-        }
-        private void MouseListener_MouseDownExt(object sender, MouseEventExtArgs e)
-        {
-            // log the mouse click
-
-            if (e.Button == System.Windows.Forms.MouseButtons.Right)
-            {
-                Console.WriteLine(thing);
-                mouse_event(MOUSEEVENTF_LEFTDOWN, e.X, e.Y, 0, 0);
-                keybd_event(0x12, 0xb8, 0, 0);
-                thing = false;
-            }
-            //else if (e.Button == System.Windows.Forms.MouseButtons.Right && !thing)
-            //{
-            //    mouse_event(MOUSEEVENTF_LEFTUP, e.X, e.Y, 0, 0);
-            //    keybd_event(0x12, 0xb8, KEYEVENTF_KEYUP, 0);
-            //    thing = true;
-            //}
-
-            // uncommenting the following line with suppress a middle mouse button click
-            // if (e.Buttons == MouseButtons.Middle) { e.Handled = true; }
         }
 
         [DllImport("user32.dll", SetLastError = true)]
@@ -97,29 +58,14 @@ namespace TrackBall
         public const int KEYEVENTF_KEYUP = 0x02;
         public const uint VK_MENU = 0x12;
 
-        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
-        public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+        [DllImport("user32.dll")]
+        public static extern void mouse_event(int dwFlags, uint dx, uint dy, int cButtons, int dwExtraInfo);
 
         private const int MOUSEEVENTF_LEFTDOWN = 0x02;
         private const int MOUSEEVENTF_LEFTUP = 0x04;
-        private void MouseListener_MouseMoveExt(object sender, MouseEventExtArgs e)
-        {
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
 
-            //if (thing && !thing2)
-            //{
-            //    mouse_event(MOUSEEVENTF_LEFTDOWN, e.X, e.Y, 0, 0);
-            //    keybd_event(0x12, 0xb8, 0, 0);
-            //    thing2 = true;
-            //}
-            //else if (!thing && thing2)
-            //{
-            //    mouse_event(MOUSEEVENTF_LEFTUP, e.X, e.Y, 0, 0);
-            //    keybd_event(0x12, 0xb8, KEYEVENTF_KEYUP, 0);
-            //    thing2 = false;
-            //}
-
-            //Console.WriteLine("Fuck off {0}", g);
-        }
     }
 
 }
